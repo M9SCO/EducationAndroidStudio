d84a0ad2c1ec590cde1310f18ac9abcc7339b909
m9sco <chr.grigorii@ya.ru>
2020-10-09 12:58:18 +0600

    end write 2 lab

--- app/auntification/src/main/java/com/example/auntification/LoginActivity.java
+++ app/auntification/src/main/java/com/example/auntification/LoginActivity.java
@@ -16,17 +16,10 @@ import java.util.Arrays;
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
-
-    static String[] USERNAME_PASSWORD = {
-            "d2abaa37a7c3db1137d385e1d8c15fd2", // log: "admin", pass: "admin"
-            "22ce1a35b913d3840f9b72fe0f87943e", // log: "m_9sco", pass: "q1w2e3"
-            "90db0030e7a6df3b0f4cba5512f72521", // log: "user", pass: "123"
-
-    };
-
     EditText writing_username;
     EditText writing_password;
     Button sign_in;
+    Button sign_up;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,24 +29,31 @@ public class LoginActivity extends AppCompatActivity {
         writing_username = (EditText) findViewById(R.id.username);
         writing_password = (EditText) findViewById(R.id.password);
         sign_in = (Button) findViewById(R.id.login);
+        sign_up = (Button) findViewById(R.id.register);
 
-        View.OnClickListener click_sign_in = new View.OnClickListener() {
+        sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String write_username = writing_username.getText().toString();
                 String write_password = writing_password.getText().toString();
                 String writing_username_password = write_username + ":" + write_password;
                 String hash = MyCrypt.toMd5(writing_username_password);
-                if (Arrays.asList(USERNAME_PASSWORD).contains(hash)) {
-                    Intent intent = new Intent(LoginActivity.this, SuccessActivity.class);
-                    startActivity(intent);
+                if (KnownUsers.getHashes().contains(hash)) {
+                    startActivity(new Intent(LoginActivity.this, SuccessActivity.class));
                 } else {
-                    Intent intent = new Intent(LoginActivity.this, FailActivity.class);
-                    startActivity(intent);
+                    startActivity(new Intent(LoginActivity.this, FailActivity.class));
                 }
             }
-        };
-        sign_in.setOnClickListener(click_sign_in );
+        });
+
+
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
 
     }
 }
